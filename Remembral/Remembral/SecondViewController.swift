7f610f414625f420fa05ac5206ceece75fc6d8c0
aayushm72 <aayushm72@gmail.com>
2018-09-21 14:45:56 -0700

    Xcode initial tabbed project

---
+++ Remembral/Remembral/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Remembral
+//
+//  Created by Aayush Malhotra on 9/21/18.
+//  Copyright © 2018 Aayush Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
