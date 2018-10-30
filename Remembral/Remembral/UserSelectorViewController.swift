5d7436789e2b441aaedb18fd4a5d1c8bfb13ed7b
Aayush Malhotra <aayushm72@gmail.com>
2018-10-30 11:04:40 -0700

    Added UI and pages for empty Caretaker

---
+++ Remembral/Remembral/UserSelectorViewController.swift
@@ -0,0 +1,39 @@
+//
+//  UserSelectorViewViewController.swift
+//  Remembral
+//
+//  Created by Aayush Malhotra on 10/29/18.
+//  Copyright © 2018 Aayush Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class UserSelectorViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func OnUserPtient(_ sender: Any) {
+    }
+    
+    @IBAction func OnUserCaretaker(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
