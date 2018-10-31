ddd5108e845fbbfec69b64fb26e571f36daa2eb8
trasmuss <trasmuss@wmc2502-mac11.ucslabs.sfu.ca>
2018-10-30 21:55:42 -0700

    do not merge

--- Remembral/Remembral/FirstViewController.swift
+++ Remembral/Remembral/FirstViewController.swift
@@ -11,7 +11,8 @@
 import UIKit
 
 class FirstViewController: UIViewController {
-
+    @IBOutlet weak var table: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
