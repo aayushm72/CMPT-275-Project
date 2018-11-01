9a18e5ad081f69cda6ccdb992ba799e673274e3b
trasmuss <trasmuss@wmc2502-mac11.ucslabs.sfu.ca>
2018-11-01 14:35:07 -0700

    Added Personal Information Table and Editing Screen

--- Remembral/Remembral/FirstViewController.swift
+++ Remembral/Remembral/FirstViewController.swift
@@ -10,8 +10,13 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-    @IBOutlet weak var table: UICollectionView!
+class FirstViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    var patientName:String = "John Doe"
+    var patientAddress:String = "123456789123 Street, Burnaby BC"
+    var patientPhoneNumber:String = "123 456 7890"
+    var caretakerName:String = "Jane Doe"
+    var caretakerPhoneNumber:String = "987 654 3210"
+    lazy var items = ["My Name:", patientName, "My Address:", patientAddress, "My Phone Number:", patientPhoneNumber, "My Caretaker:", caretakerName, "My Caretaker's Phone Number:", caretakerPhoneNumber]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +30,27 @@ class FirstViewController: UIViewController {
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
         
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+       let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! CollectionViewCell
+        
+        cell.myLabel.text = items[indexPath.item]
+        
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let EditPersonalInfoViewController = segue.destination as? EditPersonalInfoViewController {
+            EditPersonalInfoViewController.intitialPatientName = patientName
+            EditPersonalInfoViewController.intitialPatientAddress = patientAddress
+            EditPersonalInfoViewController.intitialPatientPhoneNumber = patientPhoneNumber
+            EditPersonalInfoViewController.intitialCaretakerName = caretakerName
+            EditPersonalInfoViewController.intitialCaretakerPhoneNumber = caretakerPhoneNumber
+        }
+    }
 }
 
