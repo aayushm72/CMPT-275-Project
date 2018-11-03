4885cdd0ac93e6f45e2ceab8e9498c31fef843b3
alwinl1 <alwinl@sfu.ca>
2018-11-02 21:10:10 -0700

    First and third view for caretaker

---
+++ Remembral/Remembral/CaretakerFirstViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CaretakerFirstViewController.swift
+//  Remembral
+//
+//  Created by Alwin Leong on 11/2/18.
+//  Copyright © 2018 Aayush Malhotra. All rights reserved.
+//
+
+import Foundation
---
+++ Remembral/Remembral/CaretakerThirdViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CaretakerThirdViewController.swift
+//  Remembral
+//
+//  Created by Alwin Leong on 11/2/18.
+//  Copyright © 2018 Aayush Malhotra. All rights reserved.
+//
+
+import UIKit
+
+class CaretakerThirdViewController: ThirdViewController {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
