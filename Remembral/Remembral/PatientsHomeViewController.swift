0a5c9144aaded97abcf4d62b13168e0841e07672
Aayush Malhotra <aayushm72@gmail.com>
2018-11-14 19:18:22 -0800

    SOS functionality complete with navigation and current location in text

--- Remembral/Remembral/PatientsHomeViewController.swift
+++ Remembral/Remembral/PatientsHomeViewController.swift
@@ -19,6 +19,7 @@ import UIKit
 import UserNotifications
 import MessageUI
 import MapKit
+import CoreLocation
 
 struct choices {
     static let answer1 = UNNotificationAction(identifier: "answer1", title: "Snooze" , options: UNNotificationActionOptions.foreground)
@@ -26,14 +27,15 @@ struct choices {
     static let answer2 = UNNotificationAction(identifier: "answer2", title: "Done" , options: UNNotificationActionOptions.foreground)
 }
 
-class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, MFMessageComposeViewControllerDelegate {
+class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, MFMessageComposeViewControllerDelegate, CLLocationManagerDelegate {
     var patientName:String = ""
     var patientAddress:String = ""
     var patientPhoneNumber:String = ""
     var caretakerName:String = ""
     var caretakerPhoneNumber:String = ""
     lazy var items = ["My Name:", patientName, "My Address:", patientAddress, "My Phone Number:", patientPhoneNumber, "My Caretaker:", caretakerName, "My Caretaker's Phone Number:", caretakerPhoneNumber]
-
+    let locationManager = CLLocationManager()
+    var currentLocation = CLLocationCoordinate2D()
     
     @IBOutlet weak var collectionView: UICollectionView!
     
@@ -51,7 +53,14 @@ class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UI
             self.reloadItems()
             self.collectionView.reloadData()
         }
-     
+        
+        // For use in foreground
+        self.locationManager.requestWhenInUseAuthorization()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
     }
 
     func reloadItems(){
@@ -60,48 +69,19 @@ class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UI
     @IBAction func sendSOS(_ sender: UIButton) {
         
         print(MFMessageComposeViewController.canSendText())
+        print(currentLocation)
         if (MFMessageComposeViewController.canSendText()){
             
         
             let msgVC = MFMessageComposeViewController()
         
-            msgVC.body = "SOS"
+            msgVC.body = "I need help!"
             msgVC.recipients = [FirebaseDatabase.sharedInstance.userObj.caretakerPhNo]
             msgVC.messageComposeDelegate = self
+            msgVC.addAttachmentURL(locationVCardURLFromCoordinate(coordinate: currentLocation)!, withAlternateFilename: "vCard.loc.vcf")
         
             self.present(msgVC, animated: true, completion: nil)
         }
-        
-        let sentMessage = UIAlertController(title: "Message Sent", message: "Your SOS message has been successfully sent to your Caretaker", preferredStyle: .alert)
-        let failedMessage = UIAlertController(title: "Message Sent", message: "Your SOS message could not be sent.", preferredStyle: .alert)
-        let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
-        }
-        let NavigateAction = UIAlertAction(title: "Take Me Home", style: .default) { (action:UIAlertAction!) in
-            self.performSegue(withIdentifier: "toNavigationMap", sender: self)
-        }
-        sentMessage.addAction(OKAction)
-        sentMessage.addAction(NavigateAction)
-        failedMessage.addAction(OKAction)
-//        if (result == MessageComposeResult.sent) {
-        //            self.performSegue(withIdentifier: "toNavigationMap", sender: self)
-        //do navigation here
-        //get destination latitude and longitude
-        let latitude:CLLocationDegrees = 37.748702
-        let longitude:CLLocationDegrees = -122.404118
-        //setup all options
-        let regionDistance:CLLocationDistance = 1000;
-        let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
-        let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
-        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
-                       MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
-        let placemark = MKPlacemark(coordinate: coordinates)
-        let mapItem = MKMapItem(placemark: placemark)
-        mapItem.name = "Home"
-        mapItem.openInMaps(launchOptions: options)
-//        }
-//        else if (result == MessageComposeResult.failed) {
-//            self.present(failedMessage, animated: true, completion: nil)
-//        }
     }
     
     func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
@@ -111,21 +91,33 @@ class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UI
         let OKAction = UIAlertAction(title: "OK", style: .default) { (action:UIAlertAction!) in
         }
         let NavigateAction = UIAlertAction(title: "Take Me Home", style: .default) { (action:UIAlertAction!) in
-//            self.performSegue(withIdentifier: "toNavigationMap", sender: self)
-            //do navigation here
-            //get destination latitude and longitude
-            let latitude:CLLocationDegrees = 37.748702
-            let longitude:CLLocationDegrees = -122.404118
-            //setup all options
-            let regionDistance:CLLocationDistance = 1000;
-            let coordinates = CLLocationCoordinate2DMake(latitude, longitude)
-            let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
-            let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
-                           MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
-            let placemark = MKPlacemark(coordinate: coordinates)
-            let mapItem = MKMapItem(placemark: placemark)
-            mapItem.name = "Home"
-            mapItem.openInMaps(launchOptions: options)
+            //create request for searching fome address longitude and latitude
+            let searchRequest = MKLocalSearchRequest()
+            searchRequest.naturalLanguageQuery = FirebaseDatabase.sharedInstance.userObj.address
+            
+            let activeSearch = MKLocalSearch(request: searchRequest)
+            activeSearch.start(completionHandler: {
+                (response, error) in
+                if response == nil
+                {
+                    let locationNotFoundMessage = UIAlertController(title: "Location Not found", message: "The home address location could not be found.", preferredStyle: .alert)
+                    locationNotFoundMessage.addAction(OKAction)
+                    self.present(locationNotFoundMessage, animated: true, completion: nil)
+                }
+                else{
+                    let latitude = response?.boundingRegion.center.latitude
+                    let longitude = response?.boundingRegion.center.longitude
+                    let regionDistance:CLLocationDistance = 1000;
+                    let coordinates = CLLocationCoordinate2DMake(latitude!, longitude!)
+                    let regionSpan = MKCoordinateRegionMakeWithDistance(coordinates, regionDistance, regionDistance)
+                    let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),
+                                   MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+                    let placemark = MKPlacemark(coordinate: coordinates)
+                    let mapItem = MKMapItem(placemark: placemark)
+                    mapItem.name = "Home"
+                    mapItem.openInMaps(launchOptions: options)
+                }
+            })
         }
         sentMessage.addAction(OKAction)
         sentMessage.addAction(NavigateAction)
@@ -182,5 +174,43 @@ class PatientsHomeViewController: UIViewController, UICollectionViewDelegate, UI
         reloadItems()
         collectionView.reloadData()
     }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = (manager.location?.coordinate)!
+    }
+}
+
+func locationVCardURLFromCoordinate(coordinate: CLLocationCoordinate2D) -> URL?
+{
+    guard let cachesPathString = NSSearchPathForDirectoriesInDomains(.cachesDirectory, .userDomainMask, true).first else {
+        print("Error: couldn't find the caches directory.")
+        return nil
+    }
+    
+    guard CLLocationCoordinate2DIsValid(coordinate) else {
+        print("Error: the supplied coordinate, \(coordinate), is not valid.")
+        return nil
+    }
+    
+    let vCardString = [
+        "BEGIN:VCARD",
+        "VERSION:3.0",
+        "N:;Shared Location;;;",
+        "FN:Shared Location",
+        "item1.URL;type=pref:http://maps.apple.com/?ll=\(coordinate.latitude),\(coordinate.longitude)",
+        "item1.X-ABLabel:map url",
+        "END:VCARD"
+        ].joined(separator: "\n")
+    
+    let vCardFilePath = (cachesPathString as NSString).appendingPathComponent("vCard.loc.vcf")
+    
+    do {
+        try vCardString.write(toFile: vCardFilePath, atomically: true, encoding: String.Encoding.utf8)
+    }
+    catch let error {
+        print("Error, \(error), saving vCard: \(vCardString) to file path: \(vCardFilePath).")
+    }
+    
+    return URL(fileURLWithPath: vCardFilePath)
 }
 
